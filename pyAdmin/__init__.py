1054524c4154544000019037adfb89bc4a24121a
vadimshulika <vadim.shulika4@gmail.com>
2025-05-27 22:22:00 +0300

    Add in init.py new clases

--- pyAdmin/__init__.py
+++ pyAdmin/__init__.py
@@ -1,6 +1,8 @@
 """pyAdmin: A Python package for file management and system monitoring."""
 
 from .file_manager import FileManager
+from .system_monitoring import SystemMonitor
+from .command_executor import CommandExecutor
 
 version = "0.2.0"
 all = ['FileManager', 'SystemMonitor', 'CommandExecutor']
